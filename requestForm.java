d8970f84e79a143ac9929a90994c96d5bd61959f
uchihaobito16081999 <57357856+uchihaobito16081999@users.noreply.github.com>
2019-11-07 13:15:23 +0700

    Update requestForm.java

--- requestForm.java
+++ requestForm.java
@@ -5,7 +5,9 @@ import java.awt.Font;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.ListModel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
@@ -18,6 +20,7 @@ public class requestForm extends JFrame {
 
 	private JPanel contentPane;
 
+	
 	/**
 	 * Launch the application.
 	 */
@@ -51,40 +54,49 @@ public class requestForm extends JFrame {
 		mainLabel.setBounds(6, 6, 438, 16);
 //		mainLabel.setFont(new Font());
 		contentPane.add(mainLabel);
-		
-		JList list = new JList();// cai list yeu cau ket ban
+		DefaultListModel<String> dlm = new DefaultListModel<String>();
+		dlm.addElement("ave");
+		dlm.addElement("steve");
+		JList<String> list = new JList<>(dlm);// cai list yeu cau ket ban
 		
 		list.setBounds(16, 34, 415, 194);
-		list.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				String value = (String)list.getModel().getElementAt(list.locationToIndex(e.getPoint()));
-				textNameFriend.setText(value);
-			}
-		});
-		JScrollPane scroller4List = new JScrollPane(list);
-		scroller4List.setBounds(16,34,415,194);
-		contentPane.add(scroller4List);
+		
+		
 		
 		
 		JButton accept = new JButton("accept");
 		accept.addMouseListener(new MouseAdapter() { // chap nhan ket ban thi them ban vao list friend
 			@Override
 			public void mousePressed(MouseEvent e) {
-			}
+				
+				dlm.removeElementAt(list.getSelectedIndex());	
+				}
+			
 		});
-		accept.setBounds(200, 240, 117, 29);
+		accept.setBounds(6, 240, 117, 29);
 		accept.setEnabled(false);
 		contentPane.add(accept);
+		JButton removeButton = new JButton("remove");
+		removeButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				
+				dlm.removeElementAt(list.getSelectedIndex());
+			}
+		});
+		removeButton.setEnabled(false);
+		removeButton.setBounds(314, 240, 117, 29);
+		contentPane.add(removeButton);
 		
 		JButton decline = new JButton("decline");
 		decline.addMouseListener(new MouseAdapter() {// khong dong y thi nhan nut nay de huy loi moi ket ban
 			@Override
 			public void mousePressed(MouseEvent e) {
 				
+				dlm.removeElementAt(list.getSelectedIndex());
 			}
 		});
-		decline.setBounds(314, 240, 117, 29);
+		decline.setBounds(160, 240, 117, 29);
 		decline.setEnabled(false);
 		contentPane.add(decline);
 		list.addMouseListener(new MouseAdapter() {
@@ -92,8 +104,17 @@ public class requestForm extends JFrame {
 			public void mousePressed(MouseEvent e) {
 				accept.setEnabled(true);
 				decline.setEnabled(true);
+				removeButton.setEnabled(true);
+
+				String value = (String)list.getModel().getElementAt(list.locationToIndex(e.getPoint()));
+				
+				//textNameFriend.setText(value);
 			}
-			
 		});
+		JScrollPane scroller4List = new JScrollPane(list);
+		scroller4List.setBounds(16,34,415,194);
+		contentPane.add(scroller4List);
+		
+		
 	}
 }
